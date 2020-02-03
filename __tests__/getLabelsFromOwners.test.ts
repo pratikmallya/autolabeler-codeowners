f8753f283ace9fa46337de4bf09859b7d353b3ef
pratikmallya <pratik.mallya@liveramp.com>
2020-02-02 22:54:48 -0800

    FIX: eslint fixes

--- __tests__/getLabelsFromOwners.test.ts
+++ __tests__/getLabelsFromOwners.test.ts
@@ -2,19 +2,23 @@ import {getLabelsFromOwners} from '../src/getLabelsFromOwners'
 import {Label} from '../src/getLabelsFromOwners'
 
 test('labels names are same as owners', async () => {
-  expect.assertions(3);
-  const rest: Label[] = await getLabelsFromOwners(new Set(["@Org/kelsey", "@gatsby"]))
+  expect.assertions(5)
+  const rest: Label[] = await getLabelsFromOwners(
+    new Set(['@Org/kelsey', '@gatsby'])
+  )
   expect(rest).toBeDefined()
   expect(rest.length).toBe(2)
-  expect(new Set([rest[0].name, rest[1].name])).toEqual(new Set(["@Org/kelsey", "@gatsby"]))
+  expect(new Set([rest[0].name, rest[1].name])).toEqual(
+    new Set(['@Org/kelsey', '@gatsby'])
+  )
   expect(isHexadecimal(rest[0].color)).toBeTruthy()
   expect(isHexadecimal(rest[1].color)).toBeTruthy()
 })
 
-function isHexadecimal(input:string):boolean {
-  let regexp = /^[0-9a-fA-F]+$/;
-  if (regexp.test(input)){
-      return true;
+function isHexadecimal(input: string): boolean {
+  let regexp = /^[0-9a-fA-F]+$/
+  if (regexp.test(input)) {
+    return true
   }
-  return false;        
+  return false
 }
